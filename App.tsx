4a1061f0634eeb24edb17d797b23f1f04ad55ded
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2024-02-03 15:06:46 +0700

    initial

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <CalendarPicker
+        allowRangeSelection={true}
+        onDateChange={(date: any) => console.log(date)}
+        minDate={new Date()}
+        selectedDayColor="blue"
+      />
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
