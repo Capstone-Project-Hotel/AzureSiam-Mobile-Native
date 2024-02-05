93a4c8d0063cf1353e8f85b70fd398cfa778c1be
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2024-02-05 20:59:51 +0700

    for use ui kitten

--- App.tsx
+++ App.tsx
@@ -8,28 +8,32 @@ import ReservationAndGuestDetailPage from "@/screens/ReservationAndGuestDetail";
 import SummaryBookingDetailPage from "@/screens/SummaryBookingDetail";
 import BookingConfirmationPage from "@/screens/BookingConfirmation";
 import { AppRegistry } from "react-native";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Landing" component={LandingPage} />
-        {/* <Stack.Screen name="ContactUs" component={ContactUsPage} /> */}
-        <Stack.Screen name="SearchResult" component={SearchResultPage} />
-        <Stack.Screen
-          name="ReservationAndGuestDetail"
-          component={ReservationAndGuestDetailPage}
-        />
-        <Stack.Screen
-          name="SummaryBooking"
-          component={SummaryBookingDetailPage}
-        />
-        <Stack.Screen
-          name="BookingConfirmation"
-          component={BookingConfirmationPage}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Landing" component={LandingPage} />
+          {/* <Stack.Screen name="ContactUs" component={ContactUsPage} /> */}
+          <Stack.Screen name="SearchResult" component={SearchResultPage} />
+          <Stack.Screen
+            name="ReservationAndGuestDetail"
+            component={ReservationAndGuestDetailPage}
+          />
+          <Stack.Screen
+            name="SummaryBooking"
+            component={SummaryBookingDetailPage}
+          />
+          <Stack.Screen
+            name="BookingConfirmation"
+            component={BookingConfirmationPage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
