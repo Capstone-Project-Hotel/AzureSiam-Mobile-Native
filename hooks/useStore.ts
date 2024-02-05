d94984d2885a806f662d221651838efe314d9afb
Ponrawoot Khamkhanongngam <88868999+Ponrawoot@users.noreply.github.com>
2024-02-05 17:13:08 +0700

    update store and interface

---
+++ hooks/useStore.ts
@@ -0,0 +1,99 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+const emptyGuest: Guest = {
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  gender: "",
+  birthDate: "",
+  email: "",
+  phoneNumber: "",
+  country: "",
+  city: "",
+  zipCode: "",
+  address: "",
+  id: "",
+  idType: "",
+};
+
+const emptyPaymentDetail: PaymentDetail = {
+  cardHolderName: "",
+  cardNumber: "",
+  expDate: "",
+  cvv: "",
+};
+
+const emptyBookingDetail: BookingDetail = {
+  startDate: "",
+  endDate: "",
+  adultNumber: 0,
+  childrenNumber: 0,
+  codePromotion: "",
+  standardRoomNumber: 0,
+  deluxeRoomNumber: 0,
+  familyRoomNumber: 0,
+  suiteRoomNumber: 0,
+  executiveRoomNumber: 0,
+  packageOne: false,
+  packageTwo: false,
+  isCheckedPDPA: false,
+  bookingId: "",
+  showStandard: true,
+  showDeluxe: true,
+  showFamily: true,
+  showSuite: true,
+  showExecutive: true,
+  showOnlyBalcony: false,
+  showOnlyDinnerPlan: false,
+  showOnlyJacuzzi: false,
+  showBelowOption1: false,
+  showBelowOption2: false,
+  showBelowOption3: false,
+};
+
+interface Store {
+  bookingDetail: BookingDetail;
+  guests: Guest[];
+  paymentDetail: PaymentDetail;
+  specialReq: string;
+  cardType: string;
+  exchangeRate: number;
+  currency: string;
+  setBookingDetail: (bookingDetail: BookingDetail) => void;
+  setGuests: (guests: Guest[]) => void;
+  setPaymentDetail: (paymentDetail: PaymentDetail) => void;
+  setSpecialReq: (specialReq: string) => void;
+  setCardType: (cardType: string) => void;
+  setExchangeRate: (exchangeRate: number) => void;
+  setCurrency: (currency: string) => void;
+}
+
+const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      bookingDetail: emptyBookingDetail,
+      guests: [emptyGuest],
+      paymentDetail: emptyPaymentDetail,
+      specialReq: "",
+      cardType: "",
+      exchangeRate: 1,
+      currency: "THB",
+      bookingId: "",
+      setBookingDetail: (bookingDetail: BookingDetail) =>
+        set({ bookingDetail }),
+      setGuests: (guests: Guest[]) => set({ guests }),
+      setPaymentDetail: (paymentDetail: PaymentDetail) =>
+        set({ paymentDetail }),
+      setSpecialReq: (specialReq: string) => set({ specialReq }),
+      setCardType: (cardType: string) => set({ cardType }),
+      setExchangeRate: (exchangeRate: number) => set({ exchangeRate }),
+      setCurrency: (currency: string) => set({ currency }),
+    }),
+    {
+      name: "store",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
+export default useStore;
---
+++ interfaces.ts
@@ -0,0 +1,50 @@
+interface Guest {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    gender: string;
+    birthDate: string;
+    email: string;
+    phoneNumber: string;
+    country: string;
+    city: string;
+    zipCode: string;
+    address: string;
+    id: string;
+    idType: string;
+  }
+  
+  interface PaymentDetail {
+    cardHolderName: string;
+    cardNumber: string;
+    expDate: string;
+    cvv: string;
+  }
+  
+  interface BookingDetail {
+    startDate: string;
+    endDate:string;
+    adultNumber: number;
+    childrenNumber: number;
+    codePromotion: string;
+    standardRoomNumber: number;
+    deluxeRoomNumber: number;
+    familyRoomNumber: number;
+    executiveRoomNumber: number;
+    suiteRoomNumber: number;
+    packageOne: boolean;
+    packageTwo: boolean;
+    isCheckedPDPA: boolean;
+    bookingId: string,
+    showStandard: boolean;
+    showDeluxe: boolean;
+    showFamily: boolean;
+    showSuite: boolean;
+    showExecutive: boolean;
+    showOnlyBalcony: boolean;
+    showOnlyDinnerPlan: boolean;
+    showOnlyJacuzzi: boolean;
+    showBelowOption1: boolean;
+    showBelowOption2: boolean;
+    showBelowOption3: boolean;
+  }
